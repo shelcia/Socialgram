a4662b78ba74c2d2b2fd68d252c478243b6d81a9
shelcia <shelcia1999@gmail.com>
2020-12-16 01:15:15 +0530

    update auth

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./components/styles/style.css";
 import Navbar from "./components/Navbar";
-import Login from "./components/Login";
 import Feed from "./components/Feed/Feed";
 import Profile from "./components/HomPage/Profile";
 import Settings from "./components/HomPage/Settings";
 import MyPosts from "./components/HomPage/MyPost";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AuthPage from "./components/auth/Auth";
 
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
         <div className="row" style={{ marginTop: "14vh" }}>
           <Router>
             <Switch>
-              <Route path="/" exact component={Login} />
+              <Route path="/" exact component={AuthPage} />
               <Route path="/homepage" exact component={Feed} />
               <Route path="/homepage/profile" component={Profile} />
               <Route path="/homepage/myposts" component={MyPosts} />
---
+++ src/components/auth/Auth.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Illustration from "../../assets/Illustration.png";
+import Login from "./Login";
+import Signup from "./Signup";
+
+const AuthPage = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  return (
+    <React.Fragment>
+      <div className="col-sm-9">
+        <img src={Illustration} style={{ maxHeight: "84vh" }} alt="" />
+      </div>
+      <div className="col-sm-3 form">
+        {isLogin ? (
+          <Login setIsLogin={setIsLogin} />
+        ) : (
+          <Signup setIsLogin={setIsLogin} />
+        )}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default AuthPage;
