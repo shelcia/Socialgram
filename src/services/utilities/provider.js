9df291d55983e333a5f44155de67f5eedad16615
shelcia <shelcia1999@gmail.com>
2022-11-16 03:36:05 +0530

    🚀 api migrate

--- src/services/utilities/provider.js
+++ src/services/utilities/provider.js
@@ -2,14 +2,14 @@
 
 import axios from "axios";
 import { handleResponse, handleError } from "./response";
-// import { CYCLIC_BASE_URL } from "../api";
-import { LOCALHOST_URL } from "../api";
+import { CYCLIC_BASE_URL } from "../api";
+// import { LOCALHOST_URL } from "../api";
 
 // Define your api url from any source.
 // Pulling from your .env file when on the server or from localhost when locally
-const BASE_URL = LOCALHOST_URL;
+// const BASE_URL = LOCALHOST_URL;
 
-// const BASE_URL = CYCLIC_BASE_URL;
+const BASE_URL = CYCLIC_BASE_URL;
 
 /** @param {string} resource */
 const getAll = async (resource, signal, isAuthorized = false) => {
